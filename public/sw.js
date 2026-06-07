e5497bbed399e088123da0c9de0b18dda214f46c
lucasvielpena-dev <lucasvielpena@gmail.com>
2026-06-06 23:59:53 -0300

    fix: generate and declare true PNG icons resized exactly to 192x192 and 512x512 for PWABuilder

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'ranks-hoops-cache-v11';
+const CACHE_NAME = 'ranks-hoops-cache-v12';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.jpg',
-  '/icon-512.jpg'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
